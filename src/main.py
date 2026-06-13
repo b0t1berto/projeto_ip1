fc3eedacdaf122845a491d0564e0af473df3665f
b0t_1berto <humbertoseverinodonascimentone@gmail.com>
2026-06-13 10:40:50 -0300

    missao_2

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+import gerenciador_arquivos
 continua = True
+pessoas =[]
 
 while continua:
     entrada = input().upper()
@@ -10,3 +12,20 @@ while continua:
     print("[ADICIONAR]")
     print("[REMOVER]")
     print("[SALVAR]")
+    escolha_menu = input("Digite a opção que você escolhe(ENTRE[]): ").upper()
+    if escolha_menu == "[CARREGAR]":
+        gerenciador_arquivos.carregar_pessoas("rsc/pessoas_pequeno.csv")
+        print("Pessoas carregadas com sucesso!")
+    elif escolha_menu == "[ADICIONAR]":
+        nome = input("Digite o nome da pessoa que você quer adicionar: ")
+        patrimonio = input("Digite o patrimônio da pessoa: ")
+        salario = input("Digite o salário da pessoa: ")
+        gerenciador_arquivos.adicionar_pessoas(nome, patrimonio, salario, pessoas)
+        print("Pessoa adicionada com sucesso!")
+    elif escolha_menu == "[REMOVER]":
+        nome = input("Digite o nome da pessoa que você quer remover: ")
+        gerenciador_arquivos.remover_pessoas(nome, pessoas)
+        print("Pessoa removida com sucesso!")
+    elif escolha_menu == "[SALVAR]":
+        gerenciador_arquivos.salvar_pessoas("rsc/pessoas_pequeno.csv", pessoas)
+        print("Pessoas salvas com sucesso!")
